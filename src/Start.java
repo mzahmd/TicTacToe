c3bd1f3dfb4855afefa3c10f89be2d879a9f3c82
minha <minhaz.ahmed@student.hs-rm.de>
2023-04-04 17:33:09 +0200

    Initial Commit

--- src/Start.java
+++ src/Start.java
@@ -1,20 +1,29 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
 
 class Start {
   JFrame frame = new JFrame();
   JButton startButton = new JButton("start");
   JButton closeButton = new JButton("close");
+  JLabel label = new JLabel();
   JPanel panel = new JPanel();
 
   Start() {
+    label.setText("Want to play Tic Tac Toe ? " + '\n' + "Click Start");
+    label.setFont(new Font("MV Boli", Font.BOLD, 17));
+    label.setForeground(Color.WHITE);
 
     panel.add(startButton);
+    panel.add(closeButton);
+    panel.add(label);
+    panel.setBackground(Color.BLACK);
 
     frame.add(panel);
-    frame.add(closeButton);
-    frame.setSize(200, 200);
+    frame.setSize(400, 150);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
   }
