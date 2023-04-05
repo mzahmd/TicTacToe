fdc67c7a587decb9984b77ce8606128ef9ba1d62
minha <minhaz.ahmed@student.hs-rm.de>
2023-04-05 17:42:13 +0200

    Commit

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,9 +10,9 @@ import java.awt.event.ActionListener;
 class TicTacToe implements ActionListener {
 
   JFrame frame = new JFrame();
-  JPanel panel = new JPanel();
+  JPanel button_panel = new JPanel();
   JPanel title_panel = new JPanel();
-  JLabel title = new JLabel();
+  JLabel title_label = new JLabel();
   JButton[] fields = new JButton[9];
   boolean turn;
 
@@ -20,17 +21,19 @@ class TicTacToe implements ActionListener {
     for(int i=0; i<fields.length; i++) {
       fields[i] = new JButton();
       fields[i].addActionListener(this);
-      panel.add(fields[i]);
+      button_panel.add(fields[i]);
     }
 
-    title.setText("Tic Tac Toe");
-    title.setVerticalTextPosition(JLabel.TOP);
-    title.setBounds(0, 0, 500, 0);
-    title_panel.add(title);
+    title_label.setText("Tic Tac Toe");
+    title_label.setVerticalTextPosition(JLabel.TOP);
+    title_label.setBounds(0, 0, 500, 0);
+    title_panel.setBackground(Color.RED);
+    title_panel.add(title_label);
 
-    panel.setLayout(new GridLayout(3, 3, 0, 0));
+    button_panel.setLayout(new GridLayout(3, 3, 0, 0));
+
     frame.add(title_panel);
-    frame.add(panel);
+    frame.add(button_panel);
     frame.setSize(600, 600);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
@@ -41,6 +44,7 @@ class TicTacToe implements ActionListener {
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() instanceof JButton) {
       if (turn) {
+
       } else {
 
       }
