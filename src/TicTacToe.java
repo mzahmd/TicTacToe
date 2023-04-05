5093e4becd174c4e9321e495d7f450cfc0a264f8
minha <minhaz.ahmed@student.hs-rm.de>
2023-04-05 16:51:56 +0200

    Commit

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,13 +2,13 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.GridLayout;
 
 class TicTacToe {
 
   JFrame frame = new JFrame();
+  JPanel panel = new JPanel();
+  JLabel label = new JLabel();
   JButton[] fields = new JButton[9];
 
   TicTacToe() {
@@ -18,6 +18,11 @@ class TicTacToe {
       frame.add(fields[i]);
     }
 
+    label.setText("Tic Tac Toe");
+    label.setVerticalTextPosition(JLabel.TOP);
+    panel.add(label);
+
+    frame.add(panel);
     frame.setLayout(new GridLayout(3, 3, 0, 0));
     frame.setSize(600, 600);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
