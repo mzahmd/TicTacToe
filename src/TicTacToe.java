017052620408844ea4e229016532d649a1942a39
minha <minhaz.ahmed@student.hs-rm.de>
2023-04-05 17:16:17 +0200

    Commit

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,31 +3,47 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-class TicTacToe {
+class TicTacToe implements ActionListener {
 
   JFrame frame = new JFrame();
   JPanel panel = new JPanel();
-  JLabel label = new JLabel();
+  JPanel title_panel = new JPanel();
+  JLabel title = new JLabel();
   JButton[] fields = new JButton[9];
+  boolean turn;
 
   TicTacToe() {
 
     for(int i=0; i<fields.length; i++) {
       fields[i] = new JButton();
-      frame.add(fields[i]);
+      fields[i].addActionListener(this);
+      panel.add(fields[i]);
     }
 
-    label.setText("Tic Tac Toe");
-    label.setVerticalTextPosition(JLabel.TOP);
-    panel.add(label);
+    title.setText("Tic Tac Toe");
+    title.setVerticalTextPosition(JLabel.TOP);
+    title.setBounds(0, 0, 500, 0);
+    title_panel.add(title);
 
+    panel.setLayout(new GridLayout(3, 3, 0, 0));
+    frame.add(title_panel);
     frame.add(panel);
-    frame.setLayout(new GridLayout(3, 3, 0, 0));
     frame.setSize(600, 600);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setVisible(true);
 
   }
 
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() instanceof JButton) {
+      if (turn) {
+      } else {
+
+      }
+    }
+  }
 }
